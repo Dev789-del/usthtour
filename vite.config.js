0cbdca6c4d5809ec08b237d6c6e9fdf5da2f0834
Dev789-del <huynq.bi11-113@st.usth.edu.vn>
2025-06-16 10:33:50 +0700

    Try to edit host link

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
+import { defineConfig } from 'vite'
 import restart from 'vite-plugin-restart'
 
-export default {
+export default defineConfig({
     root: 'src/', // Sources files (typically where index.html is)
     publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
     server:
     {
-        host: true, // Open to local network and display URL
+        host: 'usthtour3D.com', // Host to use and set server custom domain
         open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
     },
     build:
@@ -18,4 +19,4 @@ export default {
     [
         restart({ restart: [ '../static/**', ] }) // Restart server on static file change
     ],
-}
+});
