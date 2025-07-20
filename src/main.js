5c6cc947a87cb2b26caed056301af5aabe920548
Dev789-del <huynq.bi11-113@st.usth.edu.vn>
2025-07-20 07:49:22 +0700

    Edit the line code

--- src/main.js
+++ src/main.js
@@ -7,12 +7,7 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xA4670A);
 
 // Camera setup
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0, 2, 5);
 
 // Renderer setup
